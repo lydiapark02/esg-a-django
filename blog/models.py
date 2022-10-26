df1765a320da40a914db1ce49bc232282313b47a
lydiapark02 <noworneversyy@bagsoyeong-ui-MacBookPro.local>
2022-10-26 14:18:43 +0900

    blog.Post 모델/마이그래..

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
+# from email import contentmanager
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    created_at = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
