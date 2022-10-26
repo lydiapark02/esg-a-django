95065c67eeb961c9f9733fd9d77cbeefdb7a36f0
lydiapark02 <noworneversyy@bagsoyeong-ui-MacBookPro.local>
2022-10-26 16:11:48 +0900

    blog.index 뷰에 Post 목록 조회 추가

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from blog.models import Post
 
 def index(request):
-    return render(request, "blog/index.html")
+    # 전체 포스팅을 가져올 준비. 아직 가져오지는 않음.
+    post_qs = Post.objects.all().order_by("-id")
+    return render(request, "blog/index.html", {
+        "post_list": post_qs,
+    })
 
