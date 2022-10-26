146c55e0331a5e71553dcba077ea530b1f06cc53
lydiapark02 <noworneversyy@bagsoyeong-ui-MacBookPro.local>
2022-10-26 15:13:52 +0900

    Added updated_at to blog.Post

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,10 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.title
+        return f"[{self.pk}] {self.title}"
+
+
